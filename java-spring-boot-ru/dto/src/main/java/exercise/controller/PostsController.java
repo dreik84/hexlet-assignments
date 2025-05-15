d6d1e66f8209f6aa496a09b420f1802d7cb3457e
@hexlet/cli <support@hexlet.io>
2025-05-15 16:53:33 +0300

    submit java-spring-boot-ru/dto

---
+++ java-spring-boot-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,63 @@
+package exercise.controller;
+
+import exercise.exception.ResourceNotFoundException;
+import exercise.model.Comment;
+import exercise.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.dto.PostDTO;
+import exercise.dto.CommentDTO;
+
+// BEGIN
+@RestController
+@RequestMapping("/posts")
+public class PostsController {
+
+    @Autowired
+    private PostRepository posts;
+
+    @Autowired
+    private CommentRepository comments;
+
+    @GetMapping("")
+    public List<PostDTO> index() {
+        return posts.findAll().stream()
+                .map(this::toPostDTO)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public PostDTO show(@PathVariable long id) {
+        var post = posts.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+
+        return toPostDTO(post);
+    }
+
+    private PostDTO toPostDTO(Post post) {
+        var dto = new PostDTO();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setBody(post.getBody());
+        dto.setComments(comments.findByPostId(post.getId()).stream()
+                .map(this::toCommentDTO)
+                .toList());
+
+        return dto;
+    }
+
+    private CommentDTO toCommentDTO(Comment comment) {
+        var dto = new CommentDTO();
+        dto.setId(comment.getId());
+        dto.setBody(comment.getBody());
+        return dto;
+    }
+}
+// END
